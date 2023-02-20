3a3c154968bbc4b2cb72687667aa5ac3610e95a4
Rakamishu <mkrotify@gmail.com>
2023-02-20 15:19:44 +0200

    fixes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-require('electron-debug')({ showDevTools: true })
+// require('electron-debug')({ showDevTools: true })
 const fetch = require('node-fetch')
 const path = require("path")
 const fs = require('fs')
@@ -154,5 +154,18 @@ app.whenReady().then(() => {
     });
     
     createWindow()
+
+    app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 });
   
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
